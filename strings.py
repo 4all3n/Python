f2114a0e5972a8c71ca69a5d926e87b0e4b30406
HPSingh7146 <hpsingh7146@gmail.com>
2024-11-05 10:27:34 +0530

    added seaborn.py

---
+++ strings.py
@@ -0,0 +1,51 @@
+#String menupulation method
+
+#Example 1
+
+a = "Hello, World!"
+print(a.upper())
+
+#Example 2
+
+a = "Hello, World!"
+print(a.lower())
+
+#Example 3
+
+a = "Hello, World!"
+print(a.title())
+
+#Example 4
+
+a = "Hello, World!"
+print(a.swapcase()) 
+
+#Example 5
+
+a = "Hello, World!"
+print(a.capitalize())
+
+#Example 6
+
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+#Example 7
+
+a = "Hello, World!"
+print(a.split(","))
+
+#Example 8
+
+a = "Hello, World!"
+print(a[::-1])
+
+#Example 9
+
+a = "Hello, World!"
+print(a[2:5])
+
+#Example 10
+
+a = "Hello, World!"
+print(a[2:])
